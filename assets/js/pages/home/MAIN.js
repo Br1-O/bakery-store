666b50aebd2ca98de752890eea0996da99058afa
xanderflores <alexanderflores_10_@hotmail.es>
2024-07-30 21:10:50 -0300

    adicionamos estilos al footer

--- assets/js/pages/home/MAIN.js
+++ assets/js/pages/home/MAIN.js
@@ -8,22 +8,6 @@ export const homeContent =
 
     </section>
 
-    <section id="formation" class="position-relative d-flex flex-column justify-content-center align-items-center gap-2" style="background-color: var(--logo-bg-color);">
-
-        ${subtitle("", "var(--main-bg-color)", 2, "var(--font-hover-color)")}
-
-    </section>
-
-    <section id="projects" class="position-relative d-flex flex-column justify-content-center align-items-center">
-
-        ${subtitle("", "var(--logo-bg-color)", 2, "var(--font-hover-color)")}
-
-    </section>
-
-    <section id="contact" class="position-relative d-flex flex-column justify-content-center align-items-center" style="background-color: var(--logo-bg-color);">
-
-        ${subtitle("", "var(--main-bg-color)", 2, "var(--font-hover-color)")}
-
-    </section>
+    
 
 `;
