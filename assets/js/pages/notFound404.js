a3b9d03fd51bc9c00b7e1a722211339e9d946499
Br1-O <bruno.ortuno2@gmail.com>
2024-08-02 18:49:17 -0300

    added style for not found page

--- assets/js/pages/notFound404.js
+++ assets/js/pages/notFound404.js
@@ -1,7 +1,7 @@
 export const notFoundMessage = `
-    <div class="main" style="display:flex; flex-direction:column; justify-content:center; align-items:center; padding-top: max(10vmax, 5rem);">
-        <h4 class="subtitle"> Pagina no encontrada </h4>
-        <img src="assets/resources/img/gif/marin/marin_jump.gif" style="width: max(50%, 20rem); height:auto; border: none 1px var(--border-color); border-radius:15px; margin-top: 1.2rem" alt="">
+    <div class="main d-flex flex-column justify-content-center align-items-center flex-wrap px-2 gap-2 text-center"  style= "min-height:80vh; padding-top: max(10vmax, 10rem);">
+        <h4 class="subtitle text-wrap mx-auto" style="color:var(--font-hover-color);"> Pagina no encontrada </h4>
+        <img src="assets/resources/images/imgs/logo.png" style="width: max(40%, 20rem); height:auto; border: none 1px var(--border-color); border-radius:15px; margin-top: 1.2rem" alt="logo">
         <h4 class="subtitle"> Redireccionando... </h4>
     </div>
 `
